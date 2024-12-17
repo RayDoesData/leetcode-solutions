019858e1b3672019bef55a920450c35962f3e514
RayDoesData <raydoesdata@gmail.com>
2024-12-17 18:22:40 -0500

    Add solutions to LeetCode problem 584. Find Customer Referee

--- customer-referee.sql
+++ customer-referee.sql
@@ -24,3 +24,15 @@ Solution:
     SELECT name
     FROM customer
     WHERE referee_id !=2 or referee_id IS NULL;
+
+Output:
+
+Customer =
+    | id | name | referee_id |
+    | -- | ---- | ---------- |
+    | 1  | Will | null       |
+    | 2  | Jane | null       |
+    | 3  | Alex | 2          |
+    | 4  | Bill | null       |
+    | 5  | Zack | 1          |
+    | 6  | Mark | 2          |
