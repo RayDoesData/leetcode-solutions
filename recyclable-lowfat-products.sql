1ea313776ac943d7e7a6b586ae39a603e0619569
RayDoesData <raydoesdata@gmail.com>
2024-12-18 18:40:03 -0500

    Add solution to LeetCode problem 1757. Recyclable and Low Fat Products

--- recyclable-lowfat-products.sql
+++ recyclable-lowfat-products.sql
@@ -30,18 +30,18 @@ Solution:
 
 Result:
 
-Input
-    Products =
-    | product_id | low_fats | recyclable |
-    | ---------- | -------- | ---------- |
-    | 0          | Y        | N          |
-    | 1          | Y        | Y          |
-    | 2          | N        | Y          |
-    | 3          | Y        | Y          |
-    | 4          | N        | N          |
+    Input
+        Products =
+        | product_id | low_fats | recyclable |
+        | ---------- | -------- | ---------- |
+        | 0          | Y        | N          |
+        | 1          | Y        | Y          |
+        | 2          | N        | Y          |
+        | 3          | Y        | Y          |
+        | 4          | N        | N          |
 
-Output
-    | product_id |
-    | ---------- |
-    | 1          |
-    | 3          |
+    Output
+        | product_id |
+        | ---------- |
+        | 1          |
+        | 3          |
