bcc9a664feb0bc366c98296acebbaceec3f3d3b0
RayDoesData <raydoesdata@gmail.com>
2024-12-18 18:34:05 -0500

    Add solution to LeetCode problem 595. Big Countries

--- big-countries.py
+++ big-countries.py
@@ -35,22 +35,22 @@ Solution:
             (world['population'] >= 25_000_000)
         ][['name', 'population', 'area']]
 
-        
+
 
 Result:
 
-Input
-    World =
-    | name        | continent | area    | population | gdp          |
-    | ----------- | --------- | ------- | ---------- | ------------ |
-    | Afghanistan | Asia      | 652230  | 25500100   | 20343000000  |
-    | Albania     | Europe    | 28748   | 2831741    | 12960000000  |
-    | Algeria     | Africa    | 2381741 | 37100000   | 188681000000 |
-    | Andorra     | Europe    | 468     | 78115      | 3712000000   |
-    | Angola      | Africa    | 1246700 | 20609294   | 100990000000 
+    Input
+        World =
+        | name        | continent | area    | population | gdp          |
+        | ----------- | --------- | ------- | ---------- | ------------ |
+        | Afghanistan | Asia      | 652230  | 25500100   | 20343000000  |
+        | Albania     | Europe    | 28748   | 2831741    | 12960000000  |
+        | Algeria     | Africa    | 2381741 | 37100000   | 188681000000 |
+        | Andorra     | Europe    | 468     | 78115      | 3712000000   |
+        | Angola      | Africa    | 1246700 | 20609294   | 100990000000 
 
-Output
-    | name        | population | area    |
-    | ----------- | ---------- | ------- |
-    | Afghanistan | 25500100   | 652230  |
-    | Algeria     | 37100000   | 2381741 |
+    Output
+        | name        | population | area    |
+        | ----------- | ---------- | ------- |
+        | Afghanistan | 25500100   | 652230  |
+        | Algeria     | 37100000   | 2381741 |
