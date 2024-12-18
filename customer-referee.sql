1ceedd499f0009e772bb3ccc5762636e0dd4076d
RayDoesData <raydoesdata@gmail.com>
2024-12-18 18:36:05 -0500

    Add solution to LeetCode problem 584. Find Customer Referee

--- customer-referee.sql
+++ customer-referee.sql
@@ -29,21 +29,21 @@ Solution:
 
 Result:
 
-Input
-    Customer =
-    | id | name | referee_id |
-    | -- | ---- | ---------- |
-    | 1  | Will | null       |
-    | 2  | Jane | null       |
-    | 3  | Alex | 2          |
-    | 4  | Bill | null       |
-    | 5  | Zack | 1          |
-    | 6  | Mark | 2          |
+    Input
+        Customer =
+        | id | name | referee_id |
+        | -- | ---- | ---------- |
+        | 1  | Will | null       |
+        | 2  | Jane | null       |
+        | 3  | Alex | 2          |
+        | 4  | Bill | null       |
+        | 5  | Zack | 1          |
+        | 6  | Mark | 2          |
 
-Output
-    | name |
-    | ---- |
-    | Will |
-    | Jane |
-    | Bill |
-    | Zack |
+    Output
+        | name |
+        | ---- |
+        | Will |
+        | Jane |
+        | Bill |
+        | Zack |
