bd082cebd1376cc214b27449593c0eefa64a9c8c
RayDoesData <raydoesdata@gmail.com>
2024-12-18 18:32:07 -0500

    Add solution to LeetCode problem 1148. Article Views I

--- article-views.py
+++ article-views.py
@@ -23,30 +23,30 @@ Question:
 
 Solution:
 
-import pandas as pd
+    import pandas as pd
 
-def article_views(views: pd.DataFrame) -> pd.DataFrame:
-    return pd.DataFrame({'id':
-        sorted(views.loc[views['author_id'] == views['viewer_id']]['author_id'].unique())})
+    def article_views(views: pd.DataFrame) -> pd.DataFrame:
+        return pd.DataFrame({'id':
+            sorted(views.loc[views['author_id'] == views['viewer_id']]['author_id'].unique())})
 
 
 
 Result:
 
-Input
-    Views =
-    | article_id | author_id | viewer_id | view_date  |
-    | ---------- | --------- | --------- | ---------- |
-    | 1          | 3         | 5         | 2019-08-01 |
-    | 1          | 3         | 6         | 2019-08-02 |
-    | 2          | 7         | 7         | 2019-08-01 |
-    | 2          | 7         | 6         | 2019-08-02 |
-    | 4          | 7         | 1         | 2019-07-22 |
-    | 3          | 4         | 4         | 2019-07-21 |
-    | 3          | 4         | 4         | 2019-07-21 |
+    Input
+        Views =
+        | article_id | author_id | viewer_id | view_date  |
+        | ---------- | --------- | --------- | ---------- |
+        | 1          | 3         | 5         | 2019-08-01 |
+        | 1          | 3         | 6         | 2019-08-02 |
+        | 2          | 7         | 7         | 2019-08-01 |
+        | 2          | 7         | 6         | 2019-08-02 |
+        | 4          | 7         | 1         | 2019-07-22 |
+        | 3          | 4         | 4         | 2019-07-21 |
+        | 3          | 4         | 4         | 2019-07-21 |
 
-Output
-    | id |
-    | -- |
-    | 4  |
-    | 7  |
+    Output
+        | id |
+        | -- |
+        | 4  |
+        | 7  |
